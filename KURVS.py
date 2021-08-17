f1408fb17e4f54381601c30b493a7064dfb84978
Tom Smail <thomas.c.smail@gmail.com>
2021-08-17 16:31:35 +0100

    FInal2
    second attempt at commit

--- KURVS.py
+++ KURVS.py
@@ -3,14 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import statistics
 from scipy.optimize import curve_fit
-import math
-from scipy.sparse.extract import find
+import os
 
 
 
 def openData():
     # --- THIS CODE OPENS THE FILE 2D DATA AND RETURNS IT ---
-    image = fits.open("/Users/thoma/Desktop/cdfs_30267_cube_median_sub_2d.fits")
+    image = fits.open(os.getcwd() + "/cdfs_30267_cube_median_sub_2d.fits")
     data = image[0].data
     return data
 
@@ -19,19 +18,18 @@ def openData():
 def twoDtooneD(data):
     # --- THIS CODE IS TO TURN THE 2D DATA ARRAY INTO A 1D ARRAY ---
     brightnessList = []
-    print("This is the Data Shape")
-    print(data.shape[1])
     for i in range(data.shape[1]):
         datapoint = 0
         for j in range(data.shape[0]):
             datapoint += data[j,i]
         brightnessList.append(datapoint)
+    print(brightnessList)
     return brightnessList
 
 
 
 def averageBrightness(brightnessList, brightnessArray):
-    # --- THIS CODE IS TO AVERAGE THE BRIGHTNESS OF THE LIST OF DATA ---
+    # --- THIS CODE IS TO AVERAGE THE BRIGHTNESS OF THE LIST OF DATA AND DISPLAY IT FOR ERROR CHECKING---
     print("\n Brightness data:")
     print(brightnessList)
     print("\n Brightness data (Array):")
@@ -43,7 +41,6 @@ def averageBrightness(brightnessList, brightnessArray):
 
 def writeToGraph(brightnessArray, data): 
     # --- THIS CODE IS FOR WRTITING THE ARRAY DATA TO A SCATTER GRAPH ---
-    print("Write to graph")
     xpoints = np.arange(0, data.shape[1], 1)
     ypoints = brightnessArray
     plt.plot(xpoints, ypoints)
@@ -53,78 +50,62 @@ def writeToGraph(brightnessArray, data):
 
 def gaussian(xValues, Amp, mean, sd):
     # --- THIS CODE IS FOR PASSING THE EQUATION OF A GAUGISAN INTO "writeToGaussian" ---
-    return  Amp*np.exp(-(xValues-mean)**2/(2.*sd**2)) #this is the equation of a normal curve / gaussian 
+    return  Amp*np.exp(-(xValues-mean)**2/(2.*sd**2)) # this is the equation of a normal curve / gaussian 
 
 
 
-def writeToGaussian(brightnessList):
-    xdata = np.array(range(0, len(brightnessList))) #this creates an array of numbers in this length 0 - the length of the list
-    passedData, pcov = curve_fit(gaussian, xdata, brightnessList) #this returns a list of values to be fit to the curve (mean) 
-    plt.plot(xdata, gaussian(xdata, *passedData)) #this passes all of the attributes from passedData
-    plt.scatter(xdata, brightnessList)
+def writeToGaussian(instensityList):
+    #--- THIS CODE PASSES DATA INTO A FUNCTION WHICH IS THEN USED TO FIT A CURVE, IT ALSO MASKS ALL ROWS THAT DO NOT HAVE USABLE DATA ON THEM ---
+    xvalues = np.array(range(0, len(instensityList))) # this creates an array of numbers in this length 0 - the length of the list
+    instensityList = instensityList[12:33]
+    xdata = [i for i in xvalues if ( i >= 12 and i <= 32)] # this only takes the middle values of each row of xdata 
+    passedData, pcov = curve_fit(gaussian, xdata, instensityList, maxfev=100000, p0 = [10**-15, 25, 2]) # this returns a list of values to be fit to the curve (mean) 
+    plt.plot(xdata, gaussian(xdata, *passedData)) # this passes all of the attributes from passedData
+    plt.scatter(xdata, instensityList)
     plt.show()
-    return passedData[1] #this returns the mean of the x values. 
+    return passedData[1] # this returns the mean of the x values. 
 
 
 
 def removeHorizontalErrors(data):
     # --- THIS CODE REMOVES THE HORIZONTAL ERRORS FROM THE DATA SET, CAUSED BY INSTRUMENT ERRORS ---
     errorRows = []
-    #print("Standard Deviation of lines:")
     for y in range(data.shape[0]): # this controlls the vertical, y, direction
         row = []
         for x in range(data.shape[1]): # this controlls the horizontal, x, direction
-            row.append(data[y,x]) #appends data to rows for std 
-        print(statistics.stdev(row)) # checking the sd
+            row.append(data[y,x]) # appends data to rows for std 
         if statistics.stdev(row) >= 10**-15: # start of working out if row should be removed
             errorRows.append(y)
-    print(errorRows)
     data = np.delete(data, errorRows, 0) # removes the rows that have been identified as bad rows.
-    #print("This is the data after it has been passed through removeHorizontalErrors")
-
     return data
 
 
 
 def removeVerticalErrors(data):
     # --- THIS CODE REMOVES THE VERTICAL ERRORS FROM THE DATA SET, CAUSED BY THE ATMOSPHERE ---
-    #print("Standard Deviation of columns:")
     for x in range(data.shape[1]):# this controlls the horizontal, x, direction
         column = []
         for y in range(data.shape[0]):  # this controlls the vertical, y, direction
-            column.append(data[y,x]) #appends data to rows for std 
-        #print(statistics.stdev(column)) # checking
+            column.append(data[y,x]) # appends data to rows for std 
         data1 = np.ma.masked_greater(data, 9**-16) # start of working out if row should be removed   
-    #print("These are the columns with errors")
-    #data1 = np.delete(data, errorColumns, 0) # removes the rows that have been identified as bad rows.
-    #print("This is the data after it has been passed through removeVerticalErrors")
-    print("This is the data after removeVerticalErrors")
-    print(data1)
-    print("This is the data after removeVerticalErrors")
     return data1
 
 
 
 def findRedShift(mean):
-    meanpixle = 1.661539031658317 + (int(mean) * 0.000215820327866822)
+    meanwavelength = 1.661539031658317 + ((int(mean) -1) * 0.000215820327866822)
     emisionLineHParticle = 0.65628
-    redshift = ( meanpixle - emisionLineHParticle) / emisionLineHParticle
-    return redshift
+    redshift = ( meanwavelength - emisionLineHParticle) / emisionLineHParticle
+    return redshift, meanwavelength
 
 
 
 def main():
     data = openData()
     cleandata = removeHorizontalErrors(data)
-    print(cleandata)
-    cleanestdata = removeVerticalErrors(cleandata)
-    brightnessList = twoDtooneD(cleanestdata)
+    brightnessList = twoDtooneD(cleandata)
     mean = writeToGaussian(brightnessList)
-    print(findRedShift(mean))
-    #writeToGaussian(brightnessList, cleandata)
-    #writeToGraph(brightnessArray, cleandata )
-    #averageBrightness(brightnessList, brightnessArray)
-    #writeToGraph(brightnessArray, data)
+    
 
 
 
--- KURVSMultiSlice.py
+++ KURVSMultiSlice.py
@@ -1,4 +1,5 @@
 from KURVS import * 
+import matplotlib.pyplot as pyplot
 
 
 
@@ -6,27 +7,24 @@ def horizontalLinesErrors(data):
     # --- THIS CODE REMOVES THE HORIZONTAL ERRORS FROM THE DATA SET, CAUSED BY INSTRUMENT ERRORS ---
     errorRows = []
     slices = []
-    #print("Standard Deviation of lines:")
     for y in range(data.shape[0]): # this controlls the vertical, y, direction
         row = []
         for x in range(data.shape[1]): # this controlls the horizontal, x, direction
-            row.append(data[y,x]) #appends data to rows for std 
-        #print(statistics.stdev(row)) # checking the sd
+            row.append(data[y,x]) #appends data to rows for std
         if statistics.stdev(row) >= 10**-15: # start of working out if row should be removed
             errorRows.append(y)
-        # if the y value of a row is in error rows then start a new slice or end an old one and return the correct y values to slice the row between. 
-    print(errorRows)
     return errorRows
 
 
 
 def splitSlices(data, errorRows):
+    # --- THIS CODE SPLITS THE DATA INTO ITS DIFFERENT SLICES USING THE ERROR ROWS THAT WE HAVE PREVIOUSLY WORKED OUT ---
     cleanData = []
     slices = []
-    for y in range(data.shape[0]):     
+    for y in range(data.shape[0]): # this manovers through the y index of the tables
         if y not in errorRows:
-            cleanData.append(y)  
-    for row in cleanData:
+            cleanData.append(y)  # this makes sure the data is not anomylous 
+    for row in cleanData: # this begins to create slices 
         startOfSlice = cleanData[0]
         if row + 1 not in cleanData:
             endOfSlice = row
@@ -38,28 +36,90 @@ def splitSlices(data, errorRows):
 
     
 def sumEachSlice(slices): 
+    # --- THIS CODE SUMS EACH INDIVIDUAL SLICE AND RETURNS IT AS A LIST ---
     sliceSums = []
     for wavelengthSlice in slices:
         sliceSums.append(twoDtooneD(wavelengthSlice))
-
+    sliceSums.reverse() # this reverses the list as the data seems to be read from the bottom up
     return sliceSums
        
         
 
 def createGaussian(sliceSums):
+    # --- THIS CODE CREATES GRAPHS FOR ALL OF THE SLICES AND ALSO WORKS OUT THE REDSHIFT OF EACH SLICE ---
+    redshiftList = []
+    lambda_peak_values = []
     for array in sliceSums:
-        params = writeToGaussian(array)
+        if 3 <= sliceSums.index(array) <= 10: # only takes the middle slices as outer slices are anomylous
+            mean = writeToGaussian(array)
+            redshiftList.append(findRedShift(mean))
+            lambda_peak_values.append(findRedShift(mean)[1])
+    
+    return redshiftList, mean, lambda_peak_values
 
+
+
+def findDistanceBetweenSlices(slices):
+    sliceList = list(range(0, len(slices)))
+    mValues = []
+    for value in sliceList:
+        if 3 <= sliceList.index(value)<= 10: # only takes the middle slices as outer slices are anomylous
+            mValues.append(sliceList[value] * 1.72 * 3.086e19) 
+    return mValues
+
+
+
+def rotationCurve(lambda_peak_values, mValues):
+    velocityShifts = []
+    referenceWavelength = lambda_peak_values[3] # a wavelength to compare the others to
+    for lambda_peak_values in lambda_peak_values:
+        velocityShift = 3*10**5*(lambda_peak_values/referenceWavelength - 1)
+        velocityShifts.append(velocityShift)
+    pyplot.scatter(mValues, velocityShifts, color = "b")
+    pyplot.xlabel("Distance from centre of galaxy (km)")
+    pyplot.ylabel("Velocity Shift (km/s)")
+    pyplot.show()
+    return velocityShifts
+
+
+
+def correctingInclination(velocityShifts):
+    velocityDifferenceMax = velocityShifts[-1] - velocityShifts[0]
+    inclinationCorrection = 1.30
+    V_rot = velocityDifferenceMax * (inclinationCorrection / 2)
+    return V_rot
+
+
+
+def findMass(V_rot, mValues):
+    # THIS PIECE OF CODE FINDS THE MASS OF A GALAXY GIVEN ITS CORRECTED VELOCITY AND THE DISTANCES FROM THE CENTRE OF THE GALAXY
+    R = (mValues[-1] - mValues[0])/2
+    V_rot = V_rot * 1000
+    Mass = (V_rot**2)*R/(6.67e-11)
+    return Mass 
+
+
+
+def findDarkMatterFraction(Mass):
+    luminousMass = (0.9*10**10)*(1.989*10**30) # this is the solar mass of the galaxy timesed by the number of kilograms in a solar mass 
+    darkMatter = Mass - luminousMass 
+    darkMatterPercentage = darkMatter/Mass * 100
+    print(f"The percentage of dark matter in this galaxy is: {darkMatterPercentage} %")
+    return darkMatterPercentage 
 
 
 def main():
     data = openData()
     errorRows = horizontalLinesErrors(data)
-    cleandata = removeVerticalErrors(data)
-    slices = splitSlices(cleandata, errorRows)
+    slices = splitSlices(data, errorRows)
     sliceSums = sumEachSlice(slices)
-    print(sliceSums)
-    createGaussian(sliceSums) #Why do I have a 3D array? 
+    mValues = findDistanceBetweenSlices(slices)
+    redshiftList, mean, lambda_peak_values = createGaussian(sliceSums)
+    velocityShifts = rotationCurve(lambda_peak_values, mValues)
+    V_rot = correctingInclination(velocityShifts)
+    Mass = findMass(V_rot, mValues)
+    findDarkMatterFraction(Mass)
+
 
 
 
